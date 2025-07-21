329b6b4da44fa644ab6dcafa6bbcebeec446277b
Igor Podoselnikov <kopach007@mail.ru>
2025-07-21 21:15:04 +0500

    корректировка функции

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,39 @@
 from typing import Any
+from typing import List, Dict
+from datetime import datetime
 
 
-def filter_by_state(data: list[dict[str, Any]], user_state: str) -> list[dict[Any, Any]]:
+def filter_by_state(data: list[dict[str, Any]], state: str = 'EXECUTED') -> list[dict[Any, Any]]:
     """Функция, которая фильтрует список словарей по указанному ключу"""
     sorted_list = []
     for i in data:
-        if i.get("state", "") == user_state:
+        if i.get("state", "") == state:
             sorted_list.append(i)
     return sorted_list
 
+# Пример использования:
+sample_data = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+    ]
 
-def sort_by_date(data: list[dict[str, Any]], descending: bool = True) -> list[dict[str, Any]]:
-    """Функция сортировки списка банковских операций по дате (по умолчанию - убывание)"""
-    sorted_list = sorted(data, key=lambda x: x["date"], reverse=descending)
-    return sorted_list
+filtered_executed = filter_by_state(sample_data)
+print(filtered_executed)
+
+filtered_canceled = filter_by_state(sample_data, 'CANCELED')
+print(filtered_canceled)
+
+
+def sort_by_date(data: List[Dict], reverse: bool = True) -> List[Dict]:
+    """ Сортирует список операций по ключу 'date' """
+    # Преобразуем дату к объекту datetime, чтобы корректно отсортировать
+    return sorted(
+        data,
+        key=lambda x: datetime.fromisoformat(x['date']),
+        reverse=reverse
+    )
+
+sorted_data_desc = sort_by_date(sample_data)
+print(sorted_data_desc)
