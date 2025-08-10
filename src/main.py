be9caa8b416c575679dca39902dd8c246ee0a2ff
Igor Podoselnikov <kopach007@mail.ru>
2025-08-10 16:15:40 +0700

    test already use

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
-from src.widget import get_date
 from src.processing import filter_by_state, sort_by_date
+from src.widget import get_date
 
 result = get_mask_card_number("7000792289606361")
 print(result)
--- src/widget.py
+++ src/widget.py
@@ -1,29 +1,27 @@
 from src.masks import get_mask_account, get_mask_card_number
 
 
-def mask_account_card(account_card: str) -> str:
-    """Функция обработки информации о картах и счетах"""
-    if "счет " in account_card.lower():
-        parts = account_card.split(" ", 1)
-        account_number = parts[1]
-        mask_card = get_mask_account(account_number)
+def mask_account_card(inform_account_card: str) -> str:
+    """Функция обрабатывает информацию о картах и о счетах"""
+    if "счет" in inform_account_card.lower():
+        number_card = inform_account_card[-20:]
+        mask_card = get_mask_account(number_card)
         return f"Счет {mask_card}"
     else:
-        parts = account_card.rsplit(" ", 1)
-        card_name = parts[0]
-        card_number = parts[1]
-        mask_card = get_mask_card_number(card_number)
-        return f"{card_name} {mask_card}"
+        number_card = inform_account_card[-16:]
+        mask_card = get_mask_card_number(number_card)
+        name_bank = inform_account_card[:-16]
+        return f"{name_bank}{mask_card}"
 
 
-def get_date(format_date: str) -> str:
-    """Функция изменения формата даты"""
-    correct_date = format_date[8:10] + "." + format_date[5:7] + "." + format_date[:4]
-    return correct_date
+def get_date(formatting_date: str) -> str:
+    """Функция меняет формат даты"""
+    correct_data = formatting_date[8:10] + "." + formatting_date[5:7] + "." + formatting_date[:4]
+    return correct_data
 
 
-if __name__ == "__main__":
-    print(mask_account_card("Visa Platinum 7000792289606361"))
-    print(mask_account_card("Maestro 7000792289606361"))
-    print(mask_account_card("Счет 73654108430135874305"))
-    print(get_date("2024-03-11T02:26:18.671407"))
+# if __name__ == "__main__":
+#     print(mask_account_card("Visa Platinum 7000792289606361"))
+#     print(mask_account_card("Maestro 7000792289606361"))
+#     print(mask_account_card("Счет 73654108430135874305"))
+#     print(get_date("2024-03-11T02:26:18.671407"))
